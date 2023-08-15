a4b3865c10834f4045da0037898a5617cc7b4b68
Thiago Scarmagnani Arques <thiagoarques2@gmail.com>
2023-08-15 11:17:25 -0300

    Finalizado

--- src/Components/DashBoard/DashBoard.jsx
+++ src/Components/DashBoard/DashBoard.jsx
@@ -1,4 +1,4 @@
-import styleDash from "../Styles/dashboard.module.scss";
+import styleDash from "../Styles/dashBoard.module.scss";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
